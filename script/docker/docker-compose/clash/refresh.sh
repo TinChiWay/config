886a24dab91bf5ac771a331954b091438f368111
tinchiway <root@localhost.localdomain>
2023-03-30 14:50:59 +0800

    update

--- script/docker/docker-compose/clash/refresh.sh
+++ script/docker/docker-compose/clash/refresh.sh
@@ -1,2 +1,2 @@
-wget -O /root/project/config/script/docker/docker-compose/clash/config/clash_config.yaml https://sub.xeton.dev/sub\?target\=clash\&url\=https%3A%2F%2Fs.sublink.me%2Fsubscribe%2F3153%2FeBwMG7Nj2Ex%2Fssr%2F > logs.log
+wget -O /root/project/config/script/docker/docker-compose/clash/config/clash_config.yaml https://browser.networkwebmail.com/link/Yqaj9qDkWuwfCgQ7?clash=1 > logs.log
 sed -i 's/127.0.0.1:9090/0.0.0.0:9090/g' /root/project/config/script/docker/docker-compose/clash/config/clash_config.yaml
